c68ba58b5d8c4ecc87ce03e96e4f4af9220acce7
FethiOuail <anwarinfo26@gmail.com>
2020-08-08 17:28:40 +0100

    Update

--- app/Http/Controllers/Api/CategoriesController.php
+++ app/Http/Controllers/Api/CategoriesController.php
@@ -7,26 +7,35 @@ use App\Models\Category;
 use App\Traits\GeneralTrait;
 use Illuminate\Http\Request;
 
-class CategoriesController extends Controller {
+class CategoriesController extends Controller
+{
 
 
     use GeneralTrait;
 
-    public function index() {
+    public function index()
+    {
 
-        $categories = Category::select('id', 'name_'.app()->getLocale() . ' as name')->get();
-
+        $categories = Category::select('id', 'name_' . app()->getLocale() . ' as name')->get();
         return response()->json($categories);
 
     }
 
-    public function getCategoryById(Request $request){
+    public function getCategoryById(Request $request)
+    {
 
         $id = $request->id;
 
-        $category = Category::selection()->find($id);
+        $category = Category::select('id', 'name_' . app()->getLocale() . ' as name')->find($id);
+        // $category = Category::find($id);
 
-        return response()->json($category);
+        if (!$category) {
+           return $this->returnError('001', 'this product dont exist');
+        } else {
+            return $this->returnData('category', $category,'Successfully');
+        }
+
+
 
 
     }
